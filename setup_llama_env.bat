b9c5542d6272af68a0c3dd48b0ed457207b0dd99
kanellot <kanellot@gmail.com>
2025-05-09 21:52:00 +0200

    Setup OK

--- setup_llama_env.bat
+++ setup_llama_env.bat
@@ -6,10 +6,10 @@ set /p ENABLE_CUDA=¿Deseas soporte CUDA? (s/n):
 
 :: Preguntar por soporte CUDA
 if /I "!ENABLE_CUDA!"=="s" (
-    set "CMAKE_CUDA=--config-settings=cmake.define.LLAMA_CUDA=on"
+    set FORCE_CMAKE=1
+    set CMAKE_ARGS=-DLLAMA_CUDA=on
     echo "Soporte CUDA habilitado."
 ) else (
-    set "CMAKE_CUDA="
     echo "Soporte CUDA desactivado."
 )
 
@@ -30,46 +30,11 @@ if errorlevel 1 (
 :: Instalar herramientas básicas
 echo Instalando pip, cmake y wheel...
 ::python.exe -m pip install --upgrade pip
-pip install --upgrade pip
-pip install cmake wheel
-pip install scikit-build-core ninja
+pip install --upgrade pip --no-cache-dir --force-reinstall cmake wheel scikit-build-core ninja
 
 
-:: Inicializar submódulos si es necesario
-echo Verificando submódulos de Git...
-
-
-::
-::cmake -B build -DLLAMA_CUDA=ON
-::cmake --build build --config Release
-
-:: Instalar llama-cpp-python 
-::pip install llama-cpp-python --no-cache-dir --no-build-isolation
-set FORCE_CMAKE=1
-set CMAKE_ARGS=-DLLAMA_CUDA=on
-pip install llama-cpp-python --verbose
-pip install llama-cpp-python 
-git submodule update --init --recursive
-
-:: Clona llama.cpp si no existe
-::if not exist llama.cpp (
-::    git clone https://github.com/ggerganov/llama.cpp.git
-::)
-
-::cd llama.cpp
-
-:: Compila según respuesta
-::if /I "!ENABLE_CUDA!"=="s" (
-::    echo Compilando con soporte CUDA...
-::    cmake -B build -DLLAMA_CUDA=ON
-::    cmake --build build --config Release
-::) else (
-::    echo Compilando sin soporte CUDA...
-::    cmake -B build
-::    cmake --build build --config Release
-::)
-
-
+:: Instalar llama-cpp-python
+pip install llama-cpp-python --no-cache-dir --force-reinstall --verbose
 
 :: Mostrar resumen
 echo ========================================
